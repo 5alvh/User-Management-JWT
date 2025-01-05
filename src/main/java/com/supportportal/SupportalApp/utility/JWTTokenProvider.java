b7adc189a78d5a92ab4a2d82f0d24dc5e919ed82
Salah eddine khouadri <salahidslhddnn@gmail.com>
2025-01-06 00:07:43 +0100

    V 0.1

--- src/main/java/com/supportportal/SupportalApp/utility/JWTTokenProvider.java
+++ src/main/java/com/supportportal/SupportalApp/utility/JWTTokenProvider.java
@@ -1,12 +1,22 @@
 package com.supportportal.SupportalApp.utility;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import static com.supportportal.SupportalApp.constant.SecurityConstant.*;
+import static java.util.Arrays.stream;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.supportportal.SupportalApp.domain.UserPrincipal;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class JWTTokenProvider {
 
@@ -25,8 +35,33 @@ public class JWTTokenProvider {
                 ;
     }
 
+    public List<GrantedAuthority> getAuthorities(String token) {
+        String[] claims = getClaimsFromToken(token);
+        return stream(claims).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    private String[] getClaimsFromToken(String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return verifier.verify(token).getClaim(AUTHORITIES_KEY).asArray(String.class);
+    }
+
+    private JWTVerifier getJWTVerifier() {
+        JWTVerifier verifier;
+        try {
+            Algorithm algorithm = Algorithm.HMAC512(secret);
+            verifier = JWT.require(algorithm).withIssuer(GET_ARRAYS_LLC).withAudience(GET_ARRAYS_ADMINISTRATION).build();
+        } catch (JWTVerificationException e) {
+            throw new JWTVerificationException(TOKEN_CANNOT_BE_VERIFIED);
+        }
+        return verifier;
+    }
+
     private String[] getClaimsFromUser(UserPrincipal userPrincipal) {
-        return null;
+        List<String> authorities = new ArrayList<>();
+        for (GrantedAuthority auth: userPrincipal.getAuthorities()) {
+            authorities.add(auth.getAuthority());
+        }
+        return authorities.toArray(new String[0]);
     }
 
 
